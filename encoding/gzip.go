38b6bb2fd33a07e70878b47707c180deed587549
Yury Kozyrev <urakozz@me.com>
2016-06-07 15:09:11 +0200

    Create gzip.go

---
+++ encoding/gzip.go
@@ -0,0 +1,34 @@
+// Copyright 2016 Yury Kozyrev
+
+package encoding
+
+import (
+	"compress/flate"
+	"io"
+	"compress/gzip"
+)
+
+type EncoderDeflate struct {}
+func (_ *EncoderDeflate) GetName() (string){ return "deflate" }
+func (_ *EncoderDeflate) NewWriter(w io.Writer, level int) (ew io.Writer){
+	if level < 1 || level > 11{
+		level = 1
+	}
+	ew, _ = flate.NewWriter(w, level)
+	return
+}
+
+var _ Encoder = (*EncoderDeflate)(nil)
+
+type EncoderGzip struct {}
+func (_ *EncoderGzip) GetName() (string){ return "gzip" }
+func (_ *EncoderGzip) NewWriter(w io.Writer, level int) (ew io.Writer){
+	if level < 1 || level > 11{
+		level = 1
+	}
+	ew, _ = gzip.NewWriterLevel(w, level)
+	return
+}
+
+var _ Encoder = (*EncoderGzip)(nil)
+
